195e6c998fe044d547071dcf10e28b014ea2103b
Oleg Sviridov <olegsv3007@yandex.ru>
2021-10-08 11:38:17 +0200

    add olegsv/history package and configure it

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         App\Providers\FormServiceProvider::class,
+        \PhpClickHouseLaravel\ClickhouseServiceProvider::class,
 
         /*
          * Application Service Providers...
--- config/database.php
+++ config/database.php
@@ -91,6 +91,22 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'clickhouse' => [
+            'driver' => 'clickhouse',
+            'host' => env('CLICKHOUSE_HOST'),
+            'port' => env('CLICKHOUSE_PORT','8123'),
+            'database' => env('CLICKHOUSE_DATABASE','default'),
+            'username' => env('CLICKHOUSE_USERNAME','default'),
+            'password' => env('CLICKHOUSE_PASSWORD',''),
+            'timeout_connect' => env('CLICKHOUSE_TIMEOUT_CONNECT',2),
+            'timeout_query' => env('CLICKHOUSE_TIMEOUT_QUERY',2),
+            'https' => (bool)env('CLICKHOUSE_HTTPS', null),
+            'retries' => env('CLICKHOUSE_RETRIES', 0),
+            'settings' => [ // optional
+                'max_partitions_per_insert_block' => 300,
+            ],
+        ],
+
     ],
 
     /*
